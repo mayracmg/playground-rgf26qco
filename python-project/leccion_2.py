3947422d9c045de92344fd15fa5b03c3929c347f
Mayra Morataya <70965559+mayracmg@users.noreply.github.com>
2022-12-23 16:57:43 -0600

    Tipos de datos y variables
    ¿Qué es una Variable?
    Sirven para guardar datos en la memoria de la computadora.
    Tienen un nombre simbólico.
    
    Cada variable puede tener un tipo de dato asociado.
    TIpos de datos mas comunes:
    Integer, Float, String, Boolean

---
+++ python-project/leccion_2.py
@@ -0,0 +1,18 @@
+# Tipo de datos String -> str
+receta_1 = "Galletas de chispas de chocolate"
+receta_2 = "Alfajores"
+receta_2 = "Galletas de chocolate"
+
+#Ingredientes, tipo de datos Entero y Float -> int, float
+#Ingredientes para una receta Alfajores
+harina = 3 #libras -> Tipo de dato int
+huevos = 6 # int
+leche = 1.5 # Tipo de dato float
+
+#2 recetas de Alfajores
+lista_del_super = harina * 2 + huevos * 2 + leche * 2
+
+print("Lista del Super para las galletas: Alfajores")
+print("Harina:", harina * 2, "libras")
+print("Huevos:", huevos * 2, "unidades")
+print("Leche:", leche * 2, "tazas")
