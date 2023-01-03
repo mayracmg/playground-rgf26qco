8e41758a449cbbbdd2b64e7dc95e194f9967810e
Mayra Morataya <70965559+mayracmg@users.noreply.github.com>
2023-01-02 18:10:22 -0600

    Listas

---
+++ python-project/leccion_3.py
@@ -0,0 +1,14 @@
+ingredientes = ["1 lata de Leche Condensada", "2 barras de mantequilla derretidas", "1 taza de azúcar pulverizada", "2 tazas de maizena", "1 taza de harina", "1 cucharada de polvo de hornear", "1 huevo", "1 cucharadita de vainilla", "Almendras trituradas, coco rallado o azúcar pulverizada"]
+
+print("Lista de ingredientes")
+print(ingredientes)
+
+print("Lista ordenada de ingredientes")
+ingredientes.sort() # sort() es un metodo
+print(ingredientes)
+
+print("Cantidad de ingredientes")
+print(len(ingredientes)) #len es una funcion
+
+print("Primer y ultimo elemento de la lista")
+print(ingredientes[0], ingredientes[-1]) 
